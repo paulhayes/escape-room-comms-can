a10a4302e0a892f4eb2ff2b5ca155ad46c1a1bd1
Teo Gray <ohmiclaser@gmail.com>
2020-02-19 17:06:59 +0000

    testCode

--- sensor/src/main.ino
+++ sensor/src/main.ino
@@ -1,11 +1,89 @@
 #include <Arduino.h>
+#include <CAN.h>
 
-void setup()
-{
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
 
+  Serial.println("CAN Sender");
+
+  // start the CAN bus at 500 kbps
+  if (!CAN.begin(500E3)) {
+    Serial.println("Starting CAN failed!");
+    while (1);
+  }
 }
 
-void loop()
-{
+void loop() {
+  // send packet: id is 11 bits, packet can contain up to 8 bytes of data
+  Serial.print("Sending packet ... ");
 
+  CAN.beginPacket(0x12);
+  CAN.write('h');
+  CAN.write('e');
+  CAN.write('l');
+  CAN.write('l');
+  CAN.write('o');
+  CAN.endPacket();
+
+  Serial.println("done");
+
+  delay(1000);
+
+  // send extended packet: id is 29 bits, packet can contain up to 8 bytes of data
+  Serial.print("Sending extended packet ... ");
+
+  CAN.beginExtendedPacket(0xabcdef);
+  CAN.write('w');
+  CAN.write('o');
+  CAN.write('r');
+  CAN.write('l');
+  CAN.write('d');
+  CAN.endPacket();
+
+  Serial.println("done");
+
+  delay(1000);
 }
+/*
+void onReceive(int packetSize) {
+  // received a packet
+  Serial.print("Received ");
+
+  if (CAN.packetExtended()) {
+    
+  }
+
+  if (CAN.packetRtr()) {
+    // Remote transmission request, packet contains no data
+    CAN.beginPacket(0x12);
+    CAN.write('h');
+    CAN.write('e');
+    CAN.write('l');
+    CAN.write('l');
+    CAN.write('o');
+    CAN.endPacket();
+
+    
+  }
+
+  Serial.print("packet with id 0x");
+  Serial.print(CAN.packetId(), HEX);
+
+  if (CAN.packetRtr()) {
+    Serial.print(" and requested length ");
+    Serial.println(CAN.packetDlc());
+  } else {
+    Serial.print(" and length ");
+    Serial.println(packetSize);
+
+    // only print packet data for non-RTR packets
+    while (CAN.available()) {
+      Serial.print((char)CAN.read());
+    }
+    Serial.println();
+  }
+
+  Serial.println();
+}
+*/
